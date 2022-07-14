ecaa2bfb85bfde6d4c63dbcce2bcd658b744c5d4
rajatgarg765 <rajatrj349@gmail.com>
2022-07-14 20:53:53 +0530

    make it better

--- Icoder/settings.py
+++ Icoder/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['coders-comunity.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['geeks-community.herokuapp.com','127.0.0.1']
 
 
 # Application definition
